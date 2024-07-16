c1a09727ff2e2e94cb107058c24362c8f34598c7
Денис Ковалёв <bumercov@gmail.com>
2024-07-16 19:56:33 +0300

    Lesson_3_task1.kt Иправление

--- src/main/kotlin/Lesson3/Lesson_3_task1.kt
+++ src/main/kotlin/Lesson3/Lesson_3_task1.kt
@@ -2,10 +2,10 @@ package org.example.Lesson3
 
 fun main() {
     val username = "Денис Ковалёв"
-    val daytimeHello = "Добрый день "
-    val nightHello = "Доброй ночи "
-    var startingMessage = daytimeHello + username
+    val daytimeHello = "Добрый день"
+    val nightHello = "Доброй ночи"
+    var startingMessage = "$daytimeHello, $username"
     println(startingMessage)
-    startingMessage = nightHello + username
+    startingMessage = "$nightHello, $username"
     println(startingMessage)
 }
