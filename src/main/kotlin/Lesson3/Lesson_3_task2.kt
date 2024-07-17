eaf0df8a811390ddb9b18890e258a3ca28e560ca
Денис Ковалёв <bumercov@gmail.com>
2024-07-17 21:12:29 +0300

    Lesson_3_task2

---
+++ src/main/kotlin/Lesson3/Lesson_3_task2.kt
@@ -0,0 +1,12 @@
+package org.example.Lesson3
+
+fun main() {
+    val name = "Татьяна"
+    var family = "Андреева"
+    val surname = "Сергеевна"
+    var age = 20
+    println("$family $name $surname, $age")
+    family = "Сидорова"
+    age = 22
+    println("$family $name $surname, $age")
+}
