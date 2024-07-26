361767cafddfc40c1a05818c94b833469239ee2b
Денис Ковалёв <bumercov@gmail.com>
2024-07-26 20:10:51 +0300

    Lesson_4_task1 Исправление

--- src/main/kotlin/Lesson4/Lesson_4_task1.kt
+++ src/main/kotlin/Lesson4/Lesson_4_task1.kt
@@ -1,9 +1,10 @@
 package org.example.Lesson4
 
 fun main() {
-    val numberOfTables = 13
+
     val tableToday = 13
     val tableTomorrow = 9
-    println("Доступность столов на сегодня: ${numberOfTables > tableToday} \n" +
-            "Доступность столов на завтра: ${numberOfTables > tableTomorrow} ")
+    println("Доступность столов на сегодня: ${NUMBER_OF_TABLES > tableToday} \n" +
+            "Доступность столов на завтра: ${NUMBER_OF_TABLES > tableTomorrow} ")
 }
+const val NUMBER_OF_TABLES = 13
