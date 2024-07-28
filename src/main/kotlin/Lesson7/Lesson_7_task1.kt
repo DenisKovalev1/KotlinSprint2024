b22558bcd6a9cb2f8dee037d1efea6d4c9453a9a
Денис Ковалёв <bumercov@gmail.com>
2024-07-28 14:14:40 +0300

    Lesson_7_task1 Генерация пароля

--- src/main/kotlin/Lesson7/Lesson_7_task1.kt
+++ src/main/kotlin/Lesson7/Lesson_7_task1.kt
@@ -1,5 +1,9 @@
 package org.example.Lesson7
 
 fun main() {
-    
+    for (i in 1..3) {
+        val charRange = ('a'..'z').random()
+        val valueRange = (0..9).random()
+        print("$charRange$valueRange")
+    }
 }
