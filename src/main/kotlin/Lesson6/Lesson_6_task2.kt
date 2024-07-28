7befeb3fd2e85df87ce97d3518e3efadb21c7907
Денис Ковалёв <bumercov@gmail.com>
2024-07-28 13:06:07 +0300

    Lesson_6_task2

--- src/main/kotlin/Lesson6/Lesson_6_task2.kt
+++ src/main/kotlin/Lesson6/Lesson_6_task2.kt
@@ -2,11 +2,9 @@ package org.example.Lesson6
 
 fun main() {
     print("Введите количество секунд: ")
-    var userCounter = readln().toInt()
-    val  counterSave = userCounter
-    while (userCounter > 0){
-        userCounter--
-        Thread.sleep(1000)
-    }
+    val userCounter = readln().toLong()
+    val counterSave = userCounter
+    Thread.sleep(userCounter * 1000)
     println("Прошло $counterSave секунд")
+
 }
