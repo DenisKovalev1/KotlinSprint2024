a9839244ae5c8a6be9b2bc8029dcc744f8e90ba6
Денис Ковалёв <bumercov@gmail.com>
2024-07-29 19:51:39 +0300

    Lesson_1

--- src/main/kotlin/Lesson1/Lesson_1_task5.kt
+++ src/main/kotlin/Lesson1/Lesson_1_task5.kt
@@ -2,12 +2,14 @@ package org.example.Lesson1
 
 fun main() {
     fun timeSecondsToStr(seconds: Int): String {
-        val hour = seconds / 3600
-        val minute = (seconds % 3600) / 60
-        val second = seconds % 60
+        val hour = seconds / TIME_MIN
+        val minute = (seconds % TIME_MIN) / TIME_HOUR
+        val second = seconds % TIME_HOUR
         return String.format("%02d:%02d:%02d", hour, minute, second)
 
     }
     println("Время проведенное в космосе: ${timeSecondsToStr(TIME_IN_SPASE)}")
 }
 const val TIME_IN_SPASE = 6480
+const val TIME_MIN = 3600
+const val TIME_HOUR = 60
