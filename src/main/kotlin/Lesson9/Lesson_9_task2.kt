b95e3f374d9c676466e751d2ff40e6a6b9702019
kovalyev_ds <bumercov@gmail.com>
2024-08-01 09:19:34 +0300

    Lesson_9_task2 Исправление на lowercase()

--- src/main/kotlin/Lesson9/Lesson_9_task2.kt
+++ src/main/kotlin/Lesson9/Lesson_9_task2.kt
@@ -6,12 +6,11 @@ fun main() {
     ingredientsList.forEach { println(it) }
     println("Желаете что-то заменить?")
     val userInput = readln()
-    if (userInput.replaceFirstChar { 'Д' } == "Да") {
+    if (userInput.lowercase() == "да") {
         println("Что добавить")
         val newUserInput = readln()
         ingredientsList.add(newUserInput)
         println("Теперь в рецепте есть следующие ингредиенты:")
         ingredientsList.forEach { println(it) }
     } else println("Goodbye")
-
 }
