3b3026c8d5e132e76dd5f65a8e054b86453317c0
kovalyev_ds <bumercov@gmail.com>
2024-08-01 09:39:54 +0300

    Lesson_10_task1 Исправления

--- src/main/kotlin/Lesson10/Lesson_10_task1.kt
+++ src/main/kotlin/Lesson10/Lesson_10_task1.kt
@@ -1,17 +1,15 @@
 package org.example.Lesson10
 
-import kotlin.random.Random
-
 fun main() {
     val numberUser = game()
     val numberPC = game()
     println("Счет Пользователя : $numberUser")
     println("Счет ПК : $numberPC")
     if (numberUser > numberPC) {
-        println("Выйграл человек")
+        println("Выиграл человек")
     } else if (numberUser == numberPC) {
-        println("Нечья")
-    } else println("Выйграла машина")
+        println("Ничья")
+    } else println("Выиграла машина")
 }
 
-fun game(): Int = run { return Random.nextInt(1, 6) }
+fun game(): Int = run { return (1 .. 6).random() }
