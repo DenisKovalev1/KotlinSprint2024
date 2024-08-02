94cc321dda76fa25cb8b4d81f7b2eee3f982f280
kovalyev_ds <bumercov@gmail.com>
2024-08-02 14:28:02 +0300

    Lesson_10_task3 Исправление

--- src/main/kotlin/Lesson10/Lesson_10_task3.kt
+++ src/main/kotlin/Lesson10/Lesson_10_task3.kt
@@ -3,15 +3,19 @@ package org.example.Lesson10
 fun main() {
     println("Введите длину пароля")
     val userRead = readln().toInt()
-    getPassword(userRead)
+    println(getPassword(userRead).joinToString(""))
 }
 
-fun getPassword(lengthPassword: Int) {
-    val charRange: CharRange = (' '..'/')
+fun getPassword(lengthPassword: Int): List<Any> {
+    val listPassword: MutableList<Any> = mutableListOf()
+    val charRange = (' '..'/')
     val valueRange = (0..9)
-    for (i in 1..lengthPassword) {
-        print("${charRange.random()}${valueRange.random()}")
+    for (i in 1..lengthPassword / 2) {
+        listPassword.add(charRange.random())
+        listPassword.add(valueRange.random())
+
     }
+return listPassword
 }
 
 
