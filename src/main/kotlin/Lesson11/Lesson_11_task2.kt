f4f528e3203f31c490c99119e67e6f29da921368
Денис Ковалёв <bumercov@gmail.com>
2024-08-03 18:52:06 +0300

    Lesson_11_task2

---
+++ src/main/kotlin/Lesson11/Lesson_11_task2.kt
@@ -0,0 +1,54 @@
+package org.example.Lesson11
+
+fun main() {
+    val denis = User2(
+        id = 1,
+        login = "Denis",
+        password = "0000",
+        eMail = "sobakd@mail.ru",
+
+        )
+    denis.displayInformation()
+    println("Введите данные о себе")
+    denis.entryInBio()
+    println("Для изменения пароля введите старый пароль")
+    val userInput = readln()
+    denis.changePassword(userInput)
+    println("Информация обновлена")
+    denis.displayInformation()
+}
+
+class User2(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val eMail: String,
+    var bio: String = "",
+) {
+    fun displayInformation() {
+        println(
+            """
+        Идентификатор: $id 
+        Логин: $login
+        Пароль: $password
+        еМаил: $eMail
+        Биография: $bio
+    """.trimIndent()
+        )
+    }
+
+    fun entryInBio() {
+        val newBioRead = readln()
+        bio = newBioRead
+    }
+
+    fun changePassword(inputpassword: String) {
+        if (inputpassword == password) {
+            println("Введите новый пароль")
+            val newPasswordRead = readln()
+            password = newPasswordRead
+        } else {
+            println("Не верный пароль")
+        }
+    }
+}
