7ab44eaaffb5cea4f15441b7dcdd9d017990ea72
Денис Ковалёв <94008428+DenisKovalev1@users.noreply.github.com>
2024-08-11 21:05:42 +0300

    Delete src/main/kotlin/Lesson9/Lesson_9_task5.kt

--- src/main/kotlin/Lesson9/Lesson_9_task5.kt
+++
@@ -1,9 +0,0 @@
-package org.example.Lesson9
-
-fun main() {
-    println("Введите")
-    val list = readln().split(", ")
-   val seList = list.toSet()
-
-    println(seList.toString().substring(1))
-}
