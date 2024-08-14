facfa89b7a9fd64e3525062e6a74a35b54142a6e
Денис Ковалёв <bumercov@gmail.com>
2024-08-14 20:54:01 +0300

    Lesson_13_task3 - Первый коммит. Telephone directory 3

---
+++ src/main/kotlin/Lesson13/Lesson_13_task3.kt
@@ -0,0 +1,23 @@
+package org.example.Lesson13
+
+fun main() {
+    val users: List<telephoneDirectory2> = listOf(
+        telephoneDirectory2(name = "Денис", telephone = 89997771324),
+        telephoneDirectory2(name = "Марина", telephone = 98899229, company = null),
+        telephoneDirectory2(name = "Олег", telephone = 988997656, company = null),
+        telephoneDirectory2(name = "Кирилл", telephone = 98892345, company = "Apple"),
+        telephoneDirectory2(name = "Максим", telephone = 988777777, company = "Apple"),
+    )
+
+    users.forEach { it.informationOutput() }
+
+}
+
+class telephoneDirectory2(
+    val name: String, val telephone: Long, val company: String? = "null",
+) {
+    fun informationOutput() {
+        println("-Имя: $name \n-Номер: $telephone \n-Компания: $company")
+        println()
+    }
+}
