65162e3fe0cb5bc0267e9b49765a1c0325341f5e
Денис Ковалёв <bumercov@gmail.com>
2024-08-18 19:36:26 +0300

    Lesson_13_task5 - Исправления. Telephone directory 5

--- src/main/kotlin/Lesson13/Lesson_13_task5.kt
+++ src/main/kotlin/Lesson13/Lesson_13_task5.kt
@@ -4,19 +4,20 @@ fun main() {
     println("Введите Имя")
     val addUserName = readln()
     println("Введите номер")
-    val exceptions = try {
-        val addUserNumber = readln().toLong()
-    }catch (error: NumberFormatException){
-        println(error)
+    val addUserNumber = try {
+        val inputUserNumber = readln().toLong()
+    } catch (error: NumberFormatException) {
+        println("Ошибка - ${error::class.simpleName}")
         println("Вы ввели не цифры")
     }
     println("Введите название компании")
     val addUserCompany = readln()
     val telephoneDirectory: MutableList<telephoneDirectory5> = mutableListOf()
-    val usersContacts = telephoneDirectory5(addUserName, exceptions,addUserCompany)
+    val usersContacts = telephoneDirectory5(addUserName, addUserNumber, addUserCompany)
     telephoneDirectory.addAll(listOf(usersContacts))
     telephoneDirectory.forEach { it.informationOutput() }
 }
+
 class telephoneDirectory5(
     val name: String, val telephone: Any, val company: String? = null,
 ) {
