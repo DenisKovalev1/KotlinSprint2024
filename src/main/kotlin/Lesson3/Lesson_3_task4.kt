86277d0039ddee7d69edbc98b1bb244cfdf2a097
Денис Ковалёв <bumercov@gmail.com>
2024-08-24 16:29:43 +0300

    KS-3-4-Создал задачу. Первый коммит.

---
+++ src/main/kotlin/Lesson3/Lesson_3_task4.kt
@@ -0,0 +1,14 @@
+package org.example.Lesson3
+
+fun main() {
+    var fromWhere = "E2"
+    var toWhere = "E4"
+    var moveNumber = 1
+    var playerMove = println("откуда - $fromWhere куда - $toWhere; номер хода $moveNumber")
+    playerMove
+    fromWhere = "D2"
+    toWhere = "D3"
+    moveNumber = 2
+    playerMove = println("откуда - $fromWhere куда - $toWhere; номер хода $moveNumber")
+    playerMove
+}
