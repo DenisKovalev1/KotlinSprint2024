2e53148d0b809a2db084c3d2e9a7deadebb529e1
Денис Ковалёв <bumercov@gmail.com>
2024-08-24 19:21:42 +0300

    KS-2-5-Исправил

--- src/main/kotlin/Lesson2/Lesson_2_task5.kt
+++ src/main/kotlin/Lesson2/Lesson_2_task5.kt
@@ -3,9 +3,9 @@ package org.example.Lesson2
 import kotlin.math.pow
 
 fun main() {
-val compoundInterest = DEPOSIT_AMOUNT * (1 + INTEREST_RATE / 100).pow(DEPOSIT_TERM)
+    val depositAmount = 70000
+    val interestRate = 16.7
+    val depositTerm = 20
+    val compoundInterest = depositAmount * (1 + interestRate / 100).pow(depositTerm)
     println(String.format("%.3f", compoundInterest))
 }
-const val DEPOSIT_AMOUNT = 70000
-const val INTEREST_RATE = 16.7
-const val DEPOSIT_TERM = 20.0
