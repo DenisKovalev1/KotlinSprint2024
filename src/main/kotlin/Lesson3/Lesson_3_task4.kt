3980067e4d9fbcabaaf81fdeaeb1bf1b73c62848
Денис Ковалёв <bumercov@gmail.com>
2024-08-24 19:05:37 +0300

    KS-3-4-Исправил

--- src/main/kotlin/Lesson3/Lesson_3_task4.kt
+++ src/main/kotlin/Lesson3/Lesson_3_task4.kt
@@ -4,11 +4,10 @@ fun main() {
     var fromWhere = "E2"
     var toWhere = "E4"
     var moveNumber = 1
-    var playerMove = println("откуда - $fromWhere куда - $toWhere; номер хода $moveNumber")
-    playerMove
+    println("откуда - $fromWhere куда - $toWhere; номер хода $moveNumber")
+
     fromWhere = "D2"
     toWhere = "D3"
     moveNumber = 2
-    playerMove = println("откуда - $fromWhere куда - $toWhere; номер хода $moveNumber")
-    playerMove
+    println("откуда - $fromWhere куда - $toWhere; номер хода $moveNumber")
 }
