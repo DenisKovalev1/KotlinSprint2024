cd39c9facb3336dd159743359a487696e45dc3ce
Денис Ковалёв <bumercov@gmail.com>
2024-09-14 18:46:22 +0300

    KS-8-4. Иправление

--- src/main/kotlin/Lesson8/Lesson_8_task4.kt
+++ src/main/kotlin/Lesson8/Lesson_8_task4.kt
@@ -5,12 +5,11 @@ fun main() {
     for (i in listIngredients) println("Ингредиент ${listIngredients.indexOf(i) + 1}: $i")
     println("Какой ингредиент вы хотите поменять?")
     val userInput = readln()
-    if (listIngredients.contains(userInput)) {
-        val getIndex = listIngredients.indexOf(userInput)
+    val getIndex = listIngredients.indexOf(userInput)
+    if (getIndex == 1) {
         println("На что его заменить?")
         val userInputNew = readln()
         listIngredients.set(getIndex, userInputNew)
         listIngredients.forEach {println("Ингредиент ${listIngredients.indexOf(it) + 1}: $it") }
     } else println("Такого нет")
-
 }
