99bb27fb6cd305a0cdbadd8617cdbfd25e27fdc0
Денис Ковалёв <bumercov@gmail.com>
2024-09-15 15:45:57 +0300

    KS-16-3. Исправление

--- src/main/kotlin/Lesson16/Lesson_16_task3.kt
+++ src/main/kotlin/Lesson16/Lesson_16_task3.kt
@@ -1,17 +1,16 @@
 package org.example.Lesson16
 
 fun main() {
-    val user = User()
+    val user = User("aaa999", "Denis")
     println("Добрый день ${user.login}. Введите ваш пароль для проверки")
-    val userInputPassword = readLine()
-    println("Результат проверки: ${user.validation(userInputPassword)}")
+    val userInputPassword = readln()
+    println("Результат проверки: ${user.checkPassword(userInputPassword)}")
 }
 
-class User() {
-    private val password = "aaa999aaa"
-    val login = "Denis"
+class User(
+    private val password: String,
+    val login: String
+) {
 
-    fun validation(userInput: String?): Boolean = if (userInput == password) {
-        true
-    } else false
+    fun checkPassword(userInput: String): Boolean = userInput == password
 }
