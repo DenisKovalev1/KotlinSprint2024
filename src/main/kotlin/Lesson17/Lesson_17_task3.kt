0d7f124905204a48c084b1f1b69bd0d21dc23787
Денис Ковалёв <bumercov@gmail.com>
2024-09-16 19:42:57 +0300

    KS-17-3. Исправил.

--- src/main/kotlin/Lesson17/Lesson_17_task3.kt
+++ src/main/kotlin/Lesson17/Lesson_17_task3.kt
@@ -2,9 +2,9 @@ package org.example.Lesson17
 
 fun main() {
     val folder = Folders("Movie", 15, true)
-    println(folder.name)
+    println("${folder.name}, Количество файлов - ${folder.numberOfFiles}")
     folder.flagSecret = false
-    println(folder.name)
+    println("${folder.name}, Количество файлов - ${folder.numberOfFiles}")
 }
 
 class Folders(
@@ -13,8 +13,8 @@ class Folders(
     var flagSecret: Boolean
 ) {
     val name = name
-        get() = if (flagSecret) "Скрытая папка, Количество файлов - $numberOfFiles"
-        else "$field, Количество файлов - $numberOfFiles"
+        get() = if (flagSecret) "Скрытая папка"
+        else field
     val numberOfFiles = numberOfFiles
         get() = if (flagSecret) 0 else field
 }
