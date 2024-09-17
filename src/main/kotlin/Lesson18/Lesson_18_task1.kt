4a1fa875738719a0a298d1d7283c3593102526ba
Денис Ковалёв <bumercov@gmail.com>
2024-09-17 20:01:48 +0300

    KS-18-1. Исправил.

--- src/main/kotlin/Lesson18/Lesson_18_task1.kt
+++ src/main/kotlin/Lesson18/Lesson_18_task1.kt
@@ -1,19 +1,19 @@
 package org.example.Lesson18
 
 fun main() {
-    val user = Shop()
+    val user = Shop(2)
     user.order("Картошка")
     user.order(listOf("Помидор", "Лук", "Капуста"))
 }
 
 class Shop(
-
+var orderNumber: Int
 ) {
     fun order(product: String) {
-        println("Заказан товар $product")
+        println("Номер заказа - ${orderNumber}. Заказан товар $product")
     }
 
     fun order(product: List<String>) {
-        println("Заказаны товары $product")
+        println("Номер заказа - ${orderNumber}. Заказаны товары $product")
     }
 }
