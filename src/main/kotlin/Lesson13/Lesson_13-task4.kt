eac9fa203b1f8b8ec481df7a5c8bea3acd4f4fe3
Денис Ковалёв <bumercov@gmail.com>
2024-09-22 13:49:22 +0300

    KS-13-4. Исправление

--- src/main/kotlin/Lesson13/Lesson_13-task4.kt
+++ src/main/kotlin/Lesson13/Lesson_13-task4.kt
@@ -2,24 +2,25 @@ package org.example.Lesson13
 
 fun main() {
     val telephoneDirectory: MutableList<telephoneDirectory3> = mutableListOf()
-
-    println("Введите Имя")
-    val addUserName = readln()
-    println("Введите номер")
-    val addUserNumber = readln().toLongOrNull()?: return println("Без номера запись не добавить!")
-    println("Введите название компании")
-    val addUserCompany = readln()
-    val usersContacts = telephoneDirectory3(addUserName, addUserNumber, addUserCompany)
-    telephoneDirectory.add(usersContacts)
-
+    do {
+        println("Введите Имя")
+        val addUserName = readln()
+        println("Введите номер")
+        val addUserNumber = readln().toLongOrNull() ?: return println("Без номера запись не добавить!")
+        println("Введите название компании")
+        val addUserCompany = readln()
+        telephoneDirectory.add(telephoneDirectory3(addUserName, addUserNumber, addUserCompany))
+        println("Довавить еще номер?")
+        val userInput = readln().lowercase()
+    } while (userInput == "да")
+    println("В телефонной книге ${telephoneDirectory.size} пользователя")
     telephoneDirectory.forEach { it.informationOutput() }
-
 }
 
 class telephoneDirectory3(
     val name: String, val telephone: Long?, val company: String? = null,
 ) {
     fun informationOutput() {
-        println("-Имя: $name \n-Номер: $telephone \n-Компания: {${company}}")
+        println("-Имя: $name \n-Номер: $telephone \n-Компания: $company \n-----")
     }
 }
