20f5191c18f7f26ee0cbe26e8cdc881713816055
Денис Ковалёв <bumercov@gmail.com>
2024-10-13 20:36:37 +0300

    KS-20-3. Исправление.

--- src/main/kotlin/Lesson20/Lesson_20_task3.kt
+++ src/main/kotlin/Lesson20/Lesson_20_task3.kt
@@ -2,9 +2,9 @@ package org.example.Lesson20
 
 fun main() {
     val player = Players999("Денис", true)
-    val openTheDoor: (Players999) -> Unit =
-        fun(player: Players999) { if (player.isKey) println("Дверь открыта!") else println("Дверь заперта") }
-    openTheDoor(player)
+    val openTheDoor: (Players999) -> String =
+        { player -> if (player.isKey) "Дверь открыта!" else "Дверь заперта" }
+   println( openTheDoor(player))
 }
 
 class Players999(val name: String, val isKey: Boolean)
